fcaee73a607d4e59a320d4f7874eb0d4675d056b
Matheus Rosa <matheusrosataxa@gmail.com>
2024-08-28 01:04:15 -0300

    Docker configurado

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:16.14
+
+# Cria o diretório da aplicação no container
+RUN mkdir -p /app
+WORKDIR /app
+
+# Copia os arquivos de dependências e instala
+COPY package*.json ./
+RUN npm install
+
+# Copia o restante do código da aplicação
+COPY src src
+COPY tsconfig.json .
---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      migrations: [`${__dirname}/migrations/*{.ts,.js}`],
+      migrationsRun: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
