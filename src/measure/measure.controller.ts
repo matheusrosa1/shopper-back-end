605e244582ef7c1949fcff6dd79869e491e53264
Matheus Rosa <matheusrosataxa@gmail.com>
2024-08-29 14:08:39 -0300

    feat: measure controller

--- src/measure/measure.controller.ts
+++ src/measure/measure.controller.ts
@@ -1,17 +1,60 @@
 import {
-  Controller,
-  /*   Get,
-  Post,
   Body,
-  Patch,
+  Controller,
+  Get,
   Param,
-  Delete, */
+  Patch,
+  Post,
+  Query,
 } from '@nestjs/common';
 import { MeasureService } from './measure.service';
-/* import { CreateMeasureDto } from './dto/create-measure.dto';
-import { UpdateMeasureDto } from './dto/update-measure.dto'; */
+import { MeasureProcessorService } from './measure-processor/measure-processor.service';
 
 @Controller('measure')
 export class MeasureController {
-  constructor(private readonly measureService: MeasureService) {}
+  constructor(
+    private readonly measureService: MeasureService,
+    private readonly measureProcessorService: MeasureProcessorService,
+  ) {}
+
+  @Post('upload')
+  async uploadImageAndProcessMeasure(
+    @Body()
+    body: {
+      base64Image: string;
+      customerCode: string;
+      measureDatetime: Date;
+      measureType: 'WATER' | 'GAS';
+    },
+  ) {
+    const { base64Image, customerCode, measureDatetime, measureType } = body;
+    return await this.measureProcessorService.uploadImageAndProcessMeasure(
+      base64Image,
+      customerCode,
+      measureDatetime,
+      measureType,
+    );
+  }
+
+  @Patch('confirm')
+  async confirmMeasure(
+    @Body() body: { measureUuid: string; confirmedValue: number },
+  ) {
+    const { measureUuid, confirmedValue } = body;
+    return await this.measureService.confirmMeasure(
+      measureUuid,
+      confirmedValue,
+    );
+  }
+
+  @Get(':customerCode/list')
+  async listMeasures(
+    @Param('customerCode') customerCode: string,
+    @Query('type') measureType?: 'WATER' | 'GAS',
+  ) {
+    return await this.measureService.getMeasuresByCustomerCode(
+      customerCode,
+      measureType,
+    );
+  }
 }
